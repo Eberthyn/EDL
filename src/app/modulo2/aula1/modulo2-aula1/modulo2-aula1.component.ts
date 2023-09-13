62b9fda5172d9671138d8c3c00fdafad9e8b33b3
eberthyn <eberthyn@gmail.com>
2023-09-13 06:29:49 -0300

    Ajustes Mouseovers

--- src/app/modulo2/aula1/modulo2-aula1/modulo2-aula1.component.ts
+++ src/app/modulo2/aula1/modulo2-aula1/modulo2-aula1.component.ts
@@ -14,14 +14,14 @@ declare var UnTip: any;
 export class Modulo2Aula1Component {
 
   areas = [
-    { alt: "1", title: "<b>Nº EDL:</b><br>Número da<br> Estação Disseminadora<br> definida para o imóvel.", coords: '15,79,14' },
-    { alt: "2", title: "<b>Latitude e Longitude:</b><br>Colocar a coordenada geográfica em graus<br> decimais utilizando GPS,<br> aplicativo de georreferenciamento<br> ou base cadastral do imóvel<br> disponibilizado pelo município.", coords: '187,80,13' },
-    { alt: "3", title: "<b>Logradouro (nome, número):</b><br>Colocar o endereço<br> completo, atualizado e<br> vigente do imóvel.", coords: '338,81,14' },
-    { alt: "4", title: "<b>Quadra/Quarteirão:</b><br>Número de quadra ou<br> quarteirão utilizado pelo<br> programa de controle de<br> endemias do município.", coords: '611,80,13' },
-    { alt: "5", title: "<b>Local de instalação:</b><br>Local no imóvel onde ficou<br> a EDL, o qual foi definido<br> junto com o residente no<br> momento da implantação.", coords: '704,81,15' },
-    { alt: "6", title: "<b>Responsável do imóvel:</b><br>Pessoa que autorizou a<br> entrada ao domicílio e<br> acompanhou a instalação das EDL's.", coords: '826,80,14' },
-    { alt: "7", title: "<b>Profissional encarregado da manutenção:</b><br>Profissional de campo encarregado<br> da manutenção mensal da EDL,<br> designado pelo gestor ou<br> profissional encarregado da<br> estratégia no município.", coords: '970,79,15' },
-    { alt: "8", title: "<b>Data de instalação:</b><br>Data do dia de<br> instalação da EDL.", coords: '1115,81,13' }
+    { alt: "1", title: "Nº EDL: Número da<br>Estação Disseminadora<br>definida para o imóvel.", coords: '15,79,14' },
+    { alt: "2", title: "Latitude e Longitude: Colocar a<br>coordenada geográfica em graus<br>decimais utilizando GPS,<br>aplicativo de georreferenciamento<br>ou base cadastral do imóvel<br>disponibilizado pelo município.", coords: '187,80,13' },
+    { alt: "3", title: "Logradouro (nome, número):<br>Colocar o endereço<br>completo, atualizado e<br>vigente do imóvel.", coords: '338,81,14' },
+    { alt: "4", title: "Quadra/Quarteirão:<br>Número de quadra ou<br> quarteirão utilizado pelo<br> programa de controle de<br> endemias do município.", coords: '611,80,13' },
+    { alt: "5", title: "Local de instalação:Local<br>no imóvel onde ficou a<br>EDL, o qual foi definido<br>junto com o residente no<br>momento da implantação.", coords: '704,81,15' },
+    { alt: "6", title: "Responsável do imóvel:<br>Pessoa que autorizou a entrada<br> ao domicílio e acompanhou a<br>instalação das EDL's.", coords: '826,80,14' },
+    { alt: "7", title: "Profissional encarregado da<br> manutenção: Profissional de<br> campo encarregado da<br>manutenção mensal da EDL,<br>designado pelo gestor ou<br>profissional encarregado da<br>estratégia no município.", coords: '970,79,15' },
+    { alt: "8", title: "Data de instalação:<br>Data do dia de instalação<br> da EDL.", coords: '1115,81,13' }
   ]
 
   constructor(
--- src/app/modulo2/aula2/modulo2-aula2/modulo2-aula2.component.ts
+++ src/app/modulo2/aula2/modulo2-aula2/modulo2-aula2.component.ts
@@ -14,22 +14,22 @@ declare var UnTip: any;
 export class Modulo2Aula2Component implements AfterViewInit{
 
   areas = [
-    { alt: "a", title: "<b>Nº EDL:</b><br>Número da<br> Estação Disseminadora<br> definida para o imóvel.", coords: '13,86,22' },
-    { alt: "b", title: "<b>Logradouro (nome, número):</b><br>Colocar o endereço<br> completo, atualizado e<br> vigente do imóvel.", coords: '223,87,23' },
-    { alt: "c", title: "<b>Local de instalação:</b><br>Local no imóvel onde ficou<br> a EDL, o qual foi definido<br> junto com o residente no<br> momento da implantação.", coords: '428,87,21' },
-    { alt: "d", title: "<b>Responsável do imóvel:</b><br>Pessoa que autorizou a<br> entrada ao domicílio e<br> acompanhou a instalação das EDL's.", coords: '548,76,21' },
-    { alt: "e", title: "<b>Profissional encarregado da manutenção:</b><br>Profissional de campo encarregado<br> da manutenção mensal da EDL,<br> designado pelo gestor ou<br> profissional encarregado da<br> estratégia no município.", coords: '671,76,20' },
-    { alt: "f", title: "<b>Nível de água na EDL:</b><br>Verificar o nível de água da<br> EDL e marcar *Seca*, se a<br> EDL se encontra sem água<br> no momento da manutenção;<br> *< 50%*, se a<br> EDL se encontra com o<br> nível de água embaixo da<br> metade do pote; e *>= 50%*,<br> se a EDL se encontra com o<br> nível de água acima da<br> metade do pote.", coords: '802,69,20' },
-    { alt: "g", title: "<b>Tipo de novidades:</b><br>Marcar com X a novidade<br>apresentada no momento<br>da manutenção.", coords: '1001,69,19' },
-    { alt: "h", title: "<b>Observações:</b><br>Informações adicionais que<br>o ACE acredite necessárias<br>para complementar a vistoria<br>realizada ou condições que<br>possam comprometer a<br>qualidade da EDL.", coords: '1190,69,21' },
-    { alt: "1", title: "<b>Presença de larvas:</b><br>Presença de larvas de <i>Ae.<br> Aegypti ou Ae. Albopictus</i><br>na água contida no<br>interior da EDL.", coords: "875,148,17"},
-    { alt: "2", title: "<b>Presença de pupas:</b><br>Presença de pupas de<br><i>Ae. Aegypti ou Ae.<br>Albopictus</i> na água contida<br> no interior da EDL.", coords: "913,149,18"},
-    { alt: "3", title: "<b>Tecido desaparecido:</b><br>Ausência do tecido no<br>momento da<br>manutenção da EDL.", coords: "950,149,15"},
-    { alt: "4", title: "<b>Imóvel fechado:</b><br>No momento da visita<br>para a manutenção da<br>EDL, o imóvel se<br>encontra fechado.", coords: "986,149,16"},
-    { alt: "5", title: "<b>Estação Disseminadora<br>não instalada:</b><br>No momento da visita<br> para a manutenção da EDL,<br> o residente informou que a <br>EDL nunca foi instalada, o<br>qual pode acontecer por<br>erros no preenchimento ou<br>digitação do endereço.", coords: "1022,149,15"},
-    { alt: "6", title: "<b>Estação Disseminadora<br>desaparecida:</b><br>No momento da visita<br> para a manutenção da EDL,<br> o profissional não localiza a<br>EDL no local de instalação,<br>o qual pode acontecer<br>porque a EDL é<br>reaproveitada para outros<br>fins ou foi descartada por<br> desconhecimento do<br>objetivo da EDL no imóvel.", coords: "1056,149,15"},
-    { alt: "7", title: "<b>Estação Disseminadora<br> quebrada:</b><br>EDL danificada que<br>impeça seu bom<br>funcionamento.", coords: "1089,148,15"},
-    { alt: "8", title: "<b>Estação Disseminadora<br> retirada:</b><br>EDL retirada do imóvel<br> por desistência de<br> participação ou mudança<br> do residente, ou dificuldade<br> de acesso ao imóvel, em<br> pelo menos duas visitas<br> de manutenção.", coords: "1124,149,16"}
+    { alt: "a", title: "Nº EDL: Número da<br>Estação Disseminadora<br>definida para o imóvel.", coords: '13,86,22' },
+    { alt: "b", title: "Logradouro (nome, número):<br>Revisar se o endereço onde<br>está a EDL, está correto, se<br>não, atualizar na coluna de<br> observações.", coords: '223,87,23' },
+    { alt: "c", title: "Local de instalação: Local no<br> imóvel onde ficou a EDL, o<br> qual foi definido junto com<br> o residente no momento da<br> implantação.", coords: '428,87,21' },
+    { alt: "d", title: "Responsável do imóvel: Pessoa que<br> autorizou a entrada ao domicílio e<br> acompanhou a instalação das EDL's", coords: '548,76,21' },
+    { alt: "e", title: "Profissional encarregado da <br>manutenção: Profissional de<br> campo encarregado da<br> manutenção mensal da EDL,<br> designado pelo gestor ou<br> profissional encarregado da<br> estratégia no município.", coords: '671,76,20' },
+    { alt: "f", title: "Nível de água na EDL:<br>Verificar o nível de água <br>da EDL e marcar *Seca*,<br> se a EDL se encontra<br> sem água no momento<br> da manutenção; *< 50%*,<br> se a EDL se encontra com<br> o nível de água embaixo<br> da metade do pote; e<br> *>= 50%*, se a EDL se<br> encontra com o nível de<br> água acima da metade<br> do pote.", coords: '802,69,20' },
+    { alt: "g", title: "Tipo de novidades: Marcar<br> com X a novidade apresentada<br> no momento da manutenção.", coords: '1001,69,19' },
+    { alt: "h", title: "Observações:<br>Informações adicionais que<br>o ACE acredite necessárias<br>para complementar a vistoria<br>realizada ou condições que<br>possam comprometer a<br>qualidade da EDL.", coords: '1190,69,21' },
+    { alt: "1", title: "Presença de larvas:<br>Presença de larvas de <i>Ae.<br> Aegypti ou Ae. Albopictus</i><br>na água contida no<br>interior da EDL.", coords: "875,148,17"},
+    { alt: "2", title: "Presença de pupas:<br>Presença de pupas de<br><i>Ae. Aegypti ou Ae.<br>Albopictus</i> na água <br>contida no interior<br> da EDL.", coords: "913,149,18"},
+    { alt: "3", title: "Tecido desaparecido:<br>Ausência do tecido <br>no momento da<br>manutenção da EDL.", coords: "950,149,15"},
+    { alt: "4", title: "Imóvel fechado:<br>No momento da visita<br>para a manutenção da<br>EDL, o imóvel se<br>encontra fechado.", coords: "986,149,16"},
+    { alt: "5", title: "Estação Disseminadora<br>não instalada:<br>No momento da visita<br> para a manutenção da EDL,<br> o residente informou que a <br>EDL nunca foi instalada, o<br>qual pode acontecer por<br>erros no preenchimento ou<br>digitação do endereço.", coords: "1022,149,15"},
+    { alt: "6", title: "Estação Disseminadora<br> desaparecida:<br> No momento da visita<br> para a manutenção da<br> EDL, o profissional não <br>localiza a EDL no local<br> de instalação, o qual <br>pode acontecer porque <br>a EDL é reaproveitada <br>para outros fins ou <br>foi descartada por<br> desconhecimento do<br>objetivo da EDL no imóvel.", coords: "1056,149,15"},
+    { alt: "7", title: "Estação Disseminadora<br> quebrada:<br>EDL danificada que<br>impeça seu bom<br>funcionamento.", coords: "1089,148,15"},
+    { alt: "8", title: "Estação Disseminadora retirada:<br>EDL retirada do imóvel por<br> desistência de participação<br> ou mudança do residente,<br> ou dificuldade de acesso<br> ao imóvel, em pelo menos<br> duas visitas de manutenção.", coords: "1124,149,16"}
   ]
 
   constructor(
